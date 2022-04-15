94d583af2c63c062e71522d5a4396259df396186
Mobolaji <bjkuppy@gmail.com>
2022-04-15 05:35:20 +0100

    created the methods in Television page and also created the SamsungPage

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -8,7 +8,6 @@ public class HomePage {
     private By hamburgerMenu = By.id("nav-hamburger-menu");
     private By tvAppliancesElectronics = By.xpath("//body/div[@id='hmenu-container']/div[@id='hmenu-canvas']/div[@id='hmenu-content']/ul[1]/li[16]/a[1]");
     private By television = By.xpath("//a[contains(text(),'Televisions')]");
-    private By
 
     public  HomePage(WebDriver driver){
         this.driver = driver;
--- src/main/java/pages/TelevisionPage.java
+++ src/main/java/pages/TelevisionPage.java
@@ -1,13 +1,23 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class TelevisionPage {
     private WebDriver driver;
+    private By samsungBrand = By.xpath("//span[contains(text(),'Samsung')]");
 
     public TelevisionPage(WebDriver driver){
         this.driver = driver;
     }
+
+    public SamsungPage clickSamsungBrand(){
+        driver.findElement(samsungBrand).click();
+        return SamsungPage;
+
+    }
+
+
 
 
 }
