480511e9424f7ba8257dfa44be7a76ba2aa92ef3
Mobolaji <bjkuppy@gmail.com>
2022-04-16 16:43:29 +0100

    created a method to initialize browsers

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,15 +2,38 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import pages.HomePage;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class BaseTests {
 
     private WebDriver driver;
     protected HomePage homePage;
+
+    public WebDriver initializeBrowser(String browserName) throws MalformedURLException {
+        WebDriver driver;
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+        if (browserName.equals("chrome")){
+            dc.setBrowserName("chrome");
+        }else if (browserName.equals("firefox")){
+            dc.setBrowserName("firefox");
+        }else if (browserName.equals("edge")){
+            dc.setBrowserName("MicrosoftEdge");
+        }
+
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"),dc);
+        return driver;
+
+
+    }
 
 
     @BeforeClass
